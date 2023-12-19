aa7644931efadb08ba5ad7d5499dea8125d407d7
Alexander Jasinski <design.jasinski@gmail.com>
2023-12-19 16:12:38 +0100

    initial build

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function QuestionTimer({timeout, onTimeout}) {
+export default function QuestionTimer({timeout, onTimeout, mode}) {
 	const [remainingTime, setRemainingTime] = useState(timeout);
 	
 	useEffect(() => {
@@ -8,7 +8,7 @@ export default function QuestionTimer({timeout, onTimeout}) {
 		const timer = setTimeout(onTimeout, timeout);
 
 return () => {
-	clearTimeout();
+	clearTimeout(timer);
 };
 	}, [timeout, onTimeout]);
 
@@ -23,5 +23,5 @@ return () => {
 		clearInterval(interval);
 }, []);
 
-	return <progress id="question-time" max={timeout} value={remainingTime}/>;
+	return <progress id="question-time" max={timeout} value={remainingTime} className={mode}/>;
 }
